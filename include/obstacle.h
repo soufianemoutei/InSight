9e4f73cbc8544efe0932929a0615d51e792397d3
guemimi <34166421+guemimi@users.noreply.github.com>
2017-12-17 16:30:06 +0100

    Update distance to obstacle to 300.

--- include/obstacle.h
+++ include/obstacle.h
@@ -1,9 +1,10 @@
 #ifndef __OBSTACLE__
 #define __OBSTACLE__
 
-#define DISTANCE_FROM_OBSTACLE 100
+#define DISTANCE_FROM_OBSTACLE 300
 #define DISTANCE_TO_GO_BACK 480
 
-void detectObstacles();
+void detectObstaclesF();
+void detectObstaclesB()
 
 #endif
