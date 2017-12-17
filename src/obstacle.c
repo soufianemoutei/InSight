4ad0e9b80226d5e3e2ce2cb2cc7bb2c21f7be40f
guemimi <34166421+guemimi@users.noreply.github.com>
2017-12-17 16:44:12 +0100

    Adding function to avoid obstacles

--- src/obstacle.c
+++ src/obstacle.c
@@ -14,31 +14,52 @@
 int length;
 int weidth;
 
-void detectObstacles() {
- clock_t before, difference;
- int sec;
- stopRunning();
- printf("An obstacle was found! The distance from this obstacle is: %dmm.\n", getSonarValue());
- turnRight(0);
- sleep(2);
- if (getSonarValue > DISTANCE_TO_GO_BACK){
+void detectObstaclesF() {
+  clock_t before, difference;
+  int sec;
+  stopRunning();
+  printf("An obstacle was found! The distance from this obstacle is: %dmm.\n", getSonarValue());
+  turnRight(90);
+  sleep(2);
+  if (getSonarValue() > DISTANCE_TO_GO_BACK){
     turn(90);
+    sleep(2);
     goStraight(-1);
-    while(getSonarValue() <= DISTANCE_TO_OBSTACLE+100);
+    while(getSonarValue() <= DISTANCE_FROM_OBSTACLE);
     stopRunning();
     goStraight(0);
-    sleep(1);
     before = clock();
-    while(getSonarValue()<=DISTANCE_TO_OBSTACLE+100);
+    while(getSonarValue() <= DISTANCE_FROM_OBSTACLE);
     difference = clock() - before;
     sleep(2);
     stopRunning();
     sec = difference / CLOCKS_PER_SEC;
     printf("SEC: %d\n", sec);
-    }
+    turnLeft(90);
+    sleep(2);
+  }
 }
- 
 
+ 
+void detectObstaclesB(){
+ clock_t before, difference;
+ int sec;
+ turn(-90);
+ sleep(2);
+ turnRight(90);
+ sleep(2);
+ goStraight(0);
+ before = clock();
+ while(getSonarValue() <= DISTANCE_FROM_OBSTACLE);
+ difference = clock() - before;
+ sleep(2)
+ stopRunning();
+ sec = difference / CLOCKS_PER_SEC;
+ printf("SEC: %d\n", sec);
+ turnLeft(90);
+ sleep(2);
+ turn(-90);
+}
       
    
    /*if (getColorValue() == 5) { // BALL
