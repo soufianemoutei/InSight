172c55a90cca8eb28e5efe2b6495f7b436810159
Soufiane Moutei <soufiane.moutei@telecom-paristech.fr>
2018-01-13 11:44:28 +0100

    Update engines.h

--- include/engines.h
+++ include/engines.h
@@ -1,46 +1,23 @@
 #include "ev3.h"
 
-#ifndef __ENGINES__
-#define __ENGINES__
+#ifndef __SENSOR__
+#define __SENSOR__
 
-#define Sleep( msec ) usleep(( msec ) * 1000 )
 
-#define LEFT_PORT 68
-#define RIGHT_PORT 65
-#define BACK_PORT 66
-#define FRONT_PORT 67
-#define MAXSPEED 1050
-#define SPEED MAXSPEED/4
+typedef struct Sensors {
+   uint8_t sonarSensor;
+   uint8_t gyroSensor;
+   uint8_t colorSensor;
+   uint8_t compassSensor;
+} Sensors;
 
-typedef struct Wheel {
-  uint8_t left;
-  uint8_t right;
-  uint8_t both[3];
-} Wheel;
+extern Sensors sensors;
 
-typedef struct Engines {
-  Wheel wheelEng;
-  uint8_t backEng;
-  uint8_t frontEng;
-} Engines;
-
-extern Engines engines;
-extern int runningDirection;
-extern pthread_t movingEyesThread;
-
-void initEng();
-void goStraight(int time);
-void turn(int degree);
-void stopRunning();
-int isRunning();
-int leftWheelPosition();
-int rightWheelPosition();
-void explore();
-void backEngine(char direction);
-void exploreSmallArena();
-void goToNextCheckpoint(int currentCpId);
-void changeLayer();
-void turnSonar(int angle);
-void* move_eyes();
+void initSensors();
+int getColorValue();
+int getSonarValue();
+int getGyroValue();
+int getCompassValue();
+char* getColorName(int color);
 
 #endif
