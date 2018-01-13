0224070db1234eee6c503c53283b811110b814f4
guemimi <34166421+guemimi@users.noreply.github.com>
2018-01-13 13:16:51 +0100

    Update obstacle.h

--- include/obstacle.h
+++ include/obstacle.h
@@ -1,26 +1,13 @@
 #ifndef __OBSTACLE__
 #define __OBSTACLE__
 
-#define DISTANCE_FROM_OBSTACLE 350
+#define DISTANCE_FROM_OBSTACLE 400
 
 char closeToObstacles();
-char farObstacle();
-void detectObstacles();
-void actionAfterDetecting();
-
-
-#endif
-
-robot@ev3dev:~/ev3dev-c/source/ev3/InSight$ cat include/obstacle.h 
-#ifndef __OBSTACLE__
-#define __OBSTACLE__
-
-#define DISTANCE_FROM_OBSTACLE 350
-
-char closeToObstacles();
-char farObstacle();
-void detectObstacles();
-void actionAfterDetecting();
+//char farObstacle();
+//void detectObstacles();
+//void actionAfterDetecting();
+void BasicReaction();
 
 
 #endif
