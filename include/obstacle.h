fcf4ec76a498141d52ca346f1621f07f3277089f
akhamsi <abdelkoddouskhamsi1@gmail.com>
2018-01-13 13:14:04 +0100

    Update obstacle.h

--- include/obstacle.h
+++ include/obstacle.h
@@ -1,56 +1,13 @@
-#include <pthread.h>
-#include "ev3.h"
+#ifndef __OBSTACLE__
+#define __OBSTACLE__
 
-#ifndef __ENGINES__
-#define __ENGINES__
+#define DISTANCE_FROM_OBSTACLE 350
 
-#define Sleep( msec ) usleep(( msec ) * 1000 )
-#define DEGREE_TO_COUNT( d )  (( d ) * 170 / 90 )
-#define DEGREE_TO_TIME_FE( d )  (( d ) * 1000 / 180  )
+char closeToObstacles();
+char farObstacle();
+void detectObstacles();
+void actionAfterDetecting();
 
-#define LEFT_PORT 68
-#define RIGHT_PORT 65
-#define BACK_PORT 66
-#define FRONT_PORT 67
-#define MAXSPEED 1050
-#define SPEED_LINEAR MAXSPEED/2
-#define SPEED_CIRCULAR MAXSPEED/5
-#define SPEED_FRONT_ENGINE MAXSPEED/5
-#define SPEED_BACK_ENGINE MAXSPEED/6
-
-typedef struct Wheel {
-  uint8_t left;
-  uint8_t right;
-  uint8_t both[3];
-} Wheel;
-
-typedef struct Engines {
-  Wheel wheelEng;
-  uint8_t backEng;
-  uint8_t frontEng;
-} Engines;
-
-extern Engines engines;
-extern int runningDirection;
-extern pthread_t movingEyesThread;
-extern pthread_mutex_t turningMutex;
-extern char exploring;
-
-void initEng();
-void goStraight(int time, int direction);
-void turn(int degree);
-void stopRunning();
-int isRunning();
-int leftWheelPosition();
-int rightWheelPosition();
-void backEngine(char direction);
-void explore();
-void exploreSmallArena();
-void exploreLayer(int currentLayerID);
-void turnSonar(int angle);
-void* move_eyes();
-void cheating();
-void BasicReaction();
 
 #endif
 
@@ -67,4 +24,3 @@ void actionAfterDetecting();
 
 
 #endif
-
